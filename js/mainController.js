cdc987f301beffb156e855ff8b307b62eb98b5e0
Jaysom <mfloritvidal@gmail.com>
2016-02-04 21:19:14 +0100

    Closing to the first Stable Version
    Added fixes to the family and advantages select. There are still some
    problems with  changing the family, to be resolved.

--- js/mainController.js
+++ js/mainController.js
@@ -4,38 +4,50 @@ var TousenApp = angular.module('TousenApp', []);
 TousenApp.controller('mainController', ['$scope','CardService', function(scope, CardService){
 
 	scope.Message =  "Welcome to The TousenApp Character builder!";
+	
+	scope.vent = [];
+	scope.med = [];
+	scope.dis = [];
+	
 	scope.LoadKinds = function(){
 		CardService.getKinds()
 			.success(_handerKindsuccess)
-			.error(_handerKindError);
+			.error(_handlerError);
 	}
 
 	scope.LoadFamilies = function(){
 		CardService.getFamilies()
 			.success(_handlerFamilySuccess)
-			.error(_handlerFamilyError);
+			.error(_handlerError);
 	}
 
 	function _handerKindsuccess(res) {
 		scope.races = res.kinds;
-		scope.selectedRace = scope.races[0];
 	}
 
-	function _handerKindError(data, status) {
+	function _handlerError(data, status) {
 		console.log(data || "Request failed");
 		console.log(status);
 	}
 
 	function _handlerFamilySuccess(res) {
 		scope.families = res.Families;
-		scope.selectedFatherFamily = scope.families[0];
-		scope.selectedMotherFamily = scope.families[0];
-		
 	}
-
-	function _handlerFamilyError(data, status) {
-		console.log(data || "Request failed");
-		console.log(status);
+	
+	scope.handleFatherFamily = function(res){
+		scope.selectedFatherFamily = res;
+		scope.vent.push(Object.keys(res.vent.big));
+		scope.med.push(Object.keys(res.vent.med));
+		scope.dis.push(Object.keys(res.vent.dis))
+		console.log(res);
+	}
+	
+	scope.handleMotherFamily = function(res){
+		scope.selectedMotherFamily = res;
+		scope.vent.push(Object.keys(res.vent.big));
+		scope.med.push(Object.keys(res.vent.med));
+		scope.dis.push(Object.keys(res.vent.dis))
+		console.log(res);
 	}
 
 	scope.LoadKinds();

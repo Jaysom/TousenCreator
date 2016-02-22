ce0fa80457fc88033ebbb88e4a994ff2b511cb98
Jaysom <mfloritvidal@gmail.com>
2016-02-22 22:30:09 +0100

    Refactored many things
    Changed the App basics, for a better good.

--- js/mainController.js
+++ js/mainController.js
@@ -4,11 +4,7 @@ var TousenApp = angular.module('TousenApp', []);
 TousenApp.controller('mainController', ['$scope','CardService', function(scope, CardService){
 
 	scope.Message =  "Welcome to The TousenApp Character builder!";
-	
-	scope.vent = [];
-	scope.med = [];
-	scope.dis = [];
-	
+		
 	scope.LoadKinds = function(){
 		CardService.getKinds()
 			.success(_handerKindsuccess)
@@ -24,6 +20,10 @@ TousenApp.controller('mainController', ['$scope','CardService', function(scope, 
 	function _handerKindsuccess(res) {
 		scope.races = res.kinds;
 	}
+	
+	function handleRace(selectedRace){
+		scope.selectedRace = selectedRace;
+	}
 
 	function _handlerError(data, status) {
 		console.log(data || "Request failed");
@@ -36,18 +36,32 @@ TousenApp.controller('mainController', ['$scope','CardService', function(scope, 
 	
 	scope.handleFatherFamily = function(res){
 		scope.selectedFatherFamily = res;
-		scope.vent.push(Object.keys(res.vent.big));
-		scope.med.push(Object.keys(res.vent.med));
-		scope.dis.push(Object.keys(res.vent.dis))
+		scope.fatherVent = Object.keys(res.vent.big)[0];
+		scope.fatherMed = Object.keys(res.vent.med)[0];
+		scope.fatherDis = Object.keys(res.vent.dis)[0];
 		console.log(res);
 	}
 	
 	scope.handleMotherFamily = function(res){
 		scope.selectedMotherFamily = res;
-		scope.vent.push(Object.keys(res.vent.big));
-		scope.med.push(Object.keys(res.vent.med));
-		scope.dis.push(Object.keys(res.vent.dis))
+		scope.motherVent = Object.keys(res.vent.big)[0];
+		scope.motherMed = Object.keys(res.vent.med)[0];
+		scope.motherDis = Object.keys(res.vent.dis)[0];
 		console.log(res);
+	}
+	
+	scope.setAdv = function(){
+		console.log(this[scope.majorAvantage]);
+		scope.advSelected = scope.majorAvantage;
+		debugger;
+	}
+	
+	scope.sedMed = function(){
+		scope.medSelected = res;
+	}
+	
+	scope.setDis = function(){
+		scope.disSelected = res;
 	}
 
 	scope.LoadKinds();

f16f051d60d9a68095803c48c43986d34202f8be
Jaysom <mfloritvidal@gmail.com>
2016-03-01 21:34:51 +0100

    Bug fixing
    Closing Version 1

--- js/mainController.js
+++ js/mainController.js
@@ -9,6 +9,8 @@ TousenApp.controller('mainController', ['$scope','CardService', function(scope, 
 		CardService.getKinds()
 			.success(_handerKindsuccess)
 			.error(_handlerError);
+			
+		scope.LoadFamilies();
 	}
 
 	scope.LoadFamilies = function(){
@@ -21,29 +23,50 @@ TousenApp.controller('mainController', ['$scope','CardService', function(scope, 
 		scope.races = res.kinds;
 	}
 	
-	function handleRace(selectedRace){
+	scope.handleRace = function(selectedRace){
 		scope.selectedRace = selectedRace;
+		scope.initiative = selectedRace.attrs.int + selectedRace.attrs.ate;
 	}
 
-	function _handlerError(data, status) {
-		console.log(data || "Request failed");
-		console.log(status);
-	}
-
-	function _handlerFamilySuccess(res) {
-		scope.families = res.Families;
-	}
-	
 	scope.handleFatherFamily = function(res){
 		scope.selectedFatherFamily = res;
-		scope.fatherVent = [Object.keys(res.vent.big)[0],Object.keys(res.vent.med)[0],Object.keys(res.vent.dis)[0]];
+		scope.fatherVent = [res.vent.big.key,res.vent.med.key,res.vent.dis.key];
 	}
 	
 	scope.handleMotherFamily = function(res){
 		scope.selectedMotherFamily = res;
-		scope.motherVent = [Object.keys(res.vent.big)[0],Object.keys(res.vent.med)[0],Object.keys(res.vent.dis)[0]];
+		scope.motherVent = [res.vent.big.key,res.vent.med.key,res.vent.dis.key];
 	}
 	
+	scope.setAdv = function(){
+		scope.advSelected = scope.majorAvantage;
+		scope.vents =  [scope.selectedFatherFamily.vent.big, scope.selectedMotherFamily.vent.big];
+		scope.advDes = scope.vents.find(i => i.key == scope.advSelected);
+	}
+	
+	scope.setMed = function(){
+		scope.medSelected = scope.minorAvantage;
+		scope.meds =  [scope.selectedFatherFamily.vent.med, scope.selectedMotherFamily.vent.med];
+		scope.medDes = scope.meds.find(i => i.key == scope.medSelected);
+	}
+	
+	scope.setDis = function(){
+		scope.disSelected = scope.disAvantage;
+		scope.disavs =  [scope.selectedFatherFamily.vent.dis, scope.selectedMotherFamily.vent.dis];
+		scope.disDes = scope.disavs.find(i => i.key == scope.disSelected);
+	}	
+	
+	function _handlerError(data, status) {
+		console.log(data || "Request failed");
+		console.log(status);
+	}
+	
+	function _handlerFamilySuccess(res) {
+		scope.families = res.Families;
+		scope.honor = 0;
+		scope.richness = 0;
+	}
+	
+
 	scope.LoadKinds();
-	scope.LoadFamilies();
 }]);

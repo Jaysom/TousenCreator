b14f8c5ecc50f5c00cc08d9195ffd27ed597611f
Jaysom <mfloritvidal@gmail.com>
2016-04-12 22:42:29 +0200

    add creatures

---
+++ js/creatures.js
@@ -0,0 +1,24 @@
+{
+	"Oni": [{
+		"Raza": "Akki",
+		"effect": {
+			"reaction": "1"
+		}
+	}, {
+		"Raza": "Kijo",
+		"effect": {
+			"serenity": "1"
+		}
+	}, {
+		"Raza": "Gozuki",
+		"effect": {
+			"perseverance": "1"
+		}
+	}, {
+		"Raza": "Mezuki",
+		"effect": {
+			"atention": "1"
+		}
+	}],
+	
+}
